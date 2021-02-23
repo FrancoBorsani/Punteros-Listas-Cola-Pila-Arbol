335e5f7e1dd7e40b6e0667280c92c8d565047225
FrancoBorsani <borsafranco@gmail.com>
2021-02-23 09:35:36 -0300

    Punteros - Pasaje por referencia y por valor

---
+++ Punteros - Pasaje por referencia y por valor/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+
+//CÓDIGO ASCII
+// 65: A - 66: B - 67: C - 68 - D - 69: E - 70: F
+int func02(char a, int *b, int &c, char &d, int e, void *f){
+    *b = (int) a +1;
+        e = 71;
+        *(char*)f = (char)(e-1);
+        d= (char)c++;
+        int *aux;
+
+        aux = (int*)f;
+        f=b;
+        b=aux;
+
+        cout<<"var a: " << a <<"; var *b: "<<*b << "; var c: " << c << "; var d: "<<d<<";var e: "<< e<<";var *f: "<< *(char *)f<<endl;
+        return e;
+
+}
+
+
+int main()
+{
+int ret = 0;
+char a= 'A';
+int *b = new int;
+*b = (int) a;
+int c = 68;
+char d = 'D';
+int e = 69;
+char *f = new char;
+*f=(char) c;
+
+
+ret = func02(a, b, c, d, e, f);
+ cout<<"Segunda impresión:"<<endl;
+ cout<<"var a: " << a <<"; var *b: "<<*b << "; var c: " << c << "; var d: "<<d<<";var e: "<<
+ e<<";var *f: "<< *(char *)f<<endl;
+
+cout<<"\nvar ret: "<<ret<<endl;
+
+cout<<"\n-------------"<<endl;
+
+
+ int *p, *q;
+
+ q = & ret;
+
+ p = & ret;
+
+ if(p == q){                    // true, ambos contienen la misma posición en memoria
+    cout<<"Son iguales";
+ }else cout<<"Son distintos";
+
+
+
+
+    return 0;
+}
